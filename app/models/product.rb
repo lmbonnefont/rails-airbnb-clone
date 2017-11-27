e5f3a9b4d5f9b24c3f2d863ce23dae973ab20aee
Aurelien Allard <allard.au@gmail.com>
2017-11-27 16:13:43 +0100

    Validations added for user & product models

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,7 @@
 class Product < ApplicationRecord
+  belongs_to :user
+  validates :price, presence: true, numericality: { greater_than_or_equal: 0 }
+  validates :description, presence: true
+  validates :photo, presence: true
+  validates :location, presence: true
 end
