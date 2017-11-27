e2df37c73795516d6b3d4e91668a7ea0c68c818f
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-11-27 15:57:47 +0100

    rajout des controllers products et users + rooting

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+resources :users do
+  resources :products, only: [ :new, :create ]
+end
+
+get 'products', to: 'products#index', as: :about
+
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
