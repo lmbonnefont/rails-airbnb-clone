34937e1dd6a89f40569fed088c7e2512f91c5f9d
Aurelien Allard <allard.au@gmail.com>
2017-11-28 11:57:13 +0100

    Product form from user fully functionnal

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -46,6 +46,6 @@ class ProductsController < ApplicationController
   end
 
   def product_params
-    params.require(:product)
+    params.require(:product).permit(:price, :description, :address, :photo)
   end
 end
