053a6387e96368ceceafac6099d5ab3dc523f576
Aurelien Allard <allard.au@gmail.com>
2017-11-28 21:29:34 +0100

    Dashboard added at /users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update, :destroy]
+
+  def show
+  end
+
+  def edit
+  end
+
+  def update
+    @user.update(user_params)
+    redirect_to users_path(@users)
+  end
+
+  def destroy
+    @user.delete
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def product_params
+    params.require(:user).permit(:email, :name, :surname)
+  end
 end
