a3f1d958500e03a58ea666335076742941131864
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-11-28 15:48:55 +0100

    boutons redirigés vers les bonnes pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   devise_for :users
   resources :products, only: [ :new, :create , :index, :show, :edit, :update, :destroy ]
 
-  root to: 'pages#home'
+  root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
