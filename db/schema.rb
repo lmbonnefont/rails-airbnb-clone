572e2213895e41ece2adb2ac2f2cf398e46ca8c7
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-11-29 12:56:38 +0100

    ajout des attributes aux modèles

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171127165736) do
+ActiveRecord::Schema.define(version: 20171129114553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,6 +24,9 @@ ActiveRecord::Schema.define(version: 20171127165736) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.bigint "user_id"
+    t.string "quick_description"
+    t.string "title"
+    t.string "category"
     t.index ["user_id"], name: "index_products_on_user_id"
   end
 
@@ -42,6 +45,10 @@ ActiveRecord::Schema.define(version: 20171127165736) do
     t.inet "last_sign_in_ip"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "age"
+    t.string "profil_picture"
+    t.string "company_name"
+    t.text "failure_history"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
