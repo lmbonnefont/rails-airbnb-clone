fd913f4140abdea30a2e0b20f0ea8a415dc12403
LouisMarie Bonnefont <louis_marie.bonnefont@edu.escpeurope.eu>
2017-11-29 12:04:22 +0100

    Ajout de l'update user, redirection vers le sell

--- app/controllers/profiles_controller.rb
+++
@@ -1,6 +0,0 @@
-class ProfilesController < ApplicationController
-
-def show
-  @products = current_user.products
-end
-end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
-  resource :users, only: [ :show, :destroy]
+  resources :users, only: [ :show, :edit, :update, :destroy]
   resources :products, only: [ :new, :create , :index, :show, :edit, :update, :destroy ]
-  resource :profile, only: :show
   root to: 'products#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
