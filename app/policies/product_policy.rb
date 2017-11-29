5e35c0aeafc7c37c4432047c3ca250ccd0b3b263
Aurelien Allard <allard.au@gmail.com>
2017-11-29 12:29:18 +0100

    Adup

---
+++ app/policies/product_policy.rb
@@ -0,0 +1,24 @@
+class ProductPolicy < ApplicationPolicy
+
+  def show?
+    user.present?
+  end
+
+  def create?
+    user.present?
+  end
+
+  def update?
+    record.user == user
+  end
+
+  def destroy?
+    record.user == user
+  end
+
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+end
