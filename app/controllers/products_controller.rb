f8afcfe4e988167a78549441f926f8e0b1e23f49
Aurelien Allard <allard.au@gmail.com>
2017-11-29 11:26:33 +0100

    FIxing bugs in auth-prod

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,7 @@
 class ProductsController < ApplicationController
   before_action :set_product, only: [:show, :edit, :update, :destroy]
+  skip_before_action :authenticate_user!, only: :index
+
   # skip_after_action :verify_authorized, only: :new
 
   def index
