3716ebce22701afbe54b8123e599fbed2162bbc0
Aurelien Allard <allard.au@gmail.com>
2017-11-30 15:20:01 +0100

    Model created successfully

--- db/migrate/20171130140219_add_user_to_basket.rb
+++
@@ -1,4 +0,0 @@
-class AddUserToBasket < ActiveRecord::Migration[5.1]
-  def change
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171130135344) do
+ActiveRecord::Schema.define(version: 20171130141553) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,8 @@ ActiveRecord::Schema.define(version: 20171130135344) do
   create_table "baskets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_baskets_on_user_id"
   end
 
   create_table "products", force: :cascade do |t|
@@ -61,5 +63,6 @@ ActiveRecord::Schema.define(version: 20171130135344) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "baskets", "users"
   add_foreign_key "products", "users"
 end
