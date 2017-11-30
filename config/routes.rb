1d00209374c5615d5c745351de978f3d27a2523d
Aurelien Allard <allard.au@gmail.com>
2017-11-30 15:48:35 +0100

    Controller done

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'baskets/show'
+
   devise_for :users
-  resources :users, only: [ :show, :edit, :update, :destroy]
+  resources :users, only: [ :show, :edit, :update, :destroy] do
+    resources :baskets, only: [ :show ]
+  end
   resources :products, only: [ :new, :create , :index, :show, :edit, :update, :destroy ]
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
