1a7163694656f0da3ad62b350f621b495fc5d484
charles edange <charlesedange@hotmail.com>
2017-12-01 11:32:21 +0100

    models mig sur adress et implementation de la map

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
+  geocoded_by :address
+  after_validation :geocode, if: :address_changed?
   has_many :products, dependent: :destroy
   has_many :messages, dependent: :destroy
   has_one :basket, dependent: :destroy
